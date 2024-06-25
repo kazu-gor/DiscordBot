1986bcbe68558a32f5351ebf25185c432ce1b88f
kazu-gor <ik126@icloud.com>
2024-06-25 18:12:52 +0900

    init

---
+++ bot.py
@@ -0,0 +1,56 @@
+import os
+
+import openai
+import discord
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class ChatGPT:
+    def __init__(self, system_setting):
+        self.system = {"role": "system", "content": system_setting}
+        self.input_list = [self.system]
+        self.logs = []
+
+    def input_message(self, input_text):
+        self.input_list.append({"role": "user", "content": input_text})
+        result = openai.ChatCompletion.create(
+            model="gpt-4-turbo",
+            messages=self.input_list,
+        )
+        self.logs.append(result)
+        self.input_list.append(
+            {"role": "assistant", "content": result.choices[0].message.content}
+        )
+
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print("起動完了")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!h"):
+        question = message.content[4:]
+
+        api = ChatGPT(system_setting="あなたはアシスタントです。会話を開始します。")
+        api.input_message(question)
+
+        answer = api.input_list[-1]["content"]
+
+        await message.channel.send(answer)
+
+
+client.run(os.getenv("DISCORD_BOT_TOKEN"))

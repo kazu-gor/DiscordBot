aae737240f99c0f415bfcf22237afd3ee7179157
kazu-gor <ik126@icloud.com>
2024-11-05 16:20:35 +0900

    ✨ (Pipfile): Update openai, discord, python-dotenv, and add boto3 packages to allow for more flexibility and compatibility with different versions ♻️ (main.py): Refactor code to include a Text2speech class for text-to-speech functionality and an AmazonS3 class for handling S3 operations, improving code organization and readability 📝 (requirements.txt): Update openai and add boto3 to reflect changes made in the Pipfile for consistency and to ensure all dependencies are properly listed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import datetime
+from pathlib import Path
 
+import boto3
 import openai
 import discord
 
@@ -36,13 +39,53 @@ class ChatGPT:
         )
 
 
+class Text2speech:
+    def __init__(self) -> None:
+        pass
+
+    def text2speech(self, text):
+        self.date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        save_path = Path("/tmp/tts1.mp3")
+
+        response = openai.audio.speech.create(model="tts-1", voice="nova", input=text)
+        response.stream_to_file(save_path)
+
+        s3 = AmazonS3()
+        s3.upload(save_path)
+
+        total_str_count = len(text)
+        char_cost = 0.015 / 1000
+        return total_str_count * char_cost
+
+
+class AmazonS3:
+    def __init__(self) -> None:
+        self.boto3_client = boto3.client(
+            "s3",
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            region_name=os.getenv("AWS_REGION_NAME"),
+        )
+        self.date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+    def upload(self, file_path):
+        self.boto3_client.upload_file(
+            file_path, "discord-server", f"text_to_speech/tts1_{self.date}.mp3"
+        )
+
+    def download(self):
+        self.boto3_client.download_file(
+            "discord-server", f"text_to_speech/tts1_{self.date}.mp3", "/tmp/tts1.mp3"
+        )
+
+
 @client.event
 async def on_ready():
     print("起動完了")
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
         return
     if message.content.startswith("!h"):
@@ -51,6 +94,14 @@ async def on_message(message):
         api.input_message(question)
         answer = api.input_list[-1]["content"]
         await message.channel.send(answer)
+    if message.content.startswith("!読んで"):
+        question = message.content[4:]
+        api = Text2speech()
+        cost = api.text2speech(question)
+        await message.channel.send(
+            f"今回かかったコストは${cost:.6f}でした。",
+            file=discord.File("/tmp/tts1.mp3", filename=f"tts1_{question[:10]}.mp3"),
+        )
 
 
 if __name__ == "__main__":

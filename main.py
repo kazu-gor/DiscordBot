84d61843f24200bec0fd756518a0b495e98d0b25
kazu-gor <ik126@icloud.com>
2025-03-27 13:43:26 +0900

    fix: response;

--- main.py
+++ main.py
@@ -31,31 +31,31 @@ class ChatGPT:
                 model="gpt-4o-mini",
                 messages=self.input_list,
             )
+            self.logs.append(result)
+            self.input_list.append(
+                {"role": "assistant", "content": result.choices[0].message.content}
+            )
         except Exception as e:
             print(e)
-        self.logs.append(result)
-        self.input_list.append(
-            {"role": "assistant", "content": result.choices[0].message.content}
-        )
 
 
-class Text2speech:
-    def __init__(self) -> None:
-        pass
+# class Text2speech:
+#     def __init__(self) -> None:
+#         pass
 
-    def text2speech(self, text):
-        self.date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        save_path = Path("/tmp/tts1.mp3")
+#     def text2speech(self, text):
+#         self.date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+#         save_path = Path("/tmp/tts1.mp3")
 
-        response = openai.audio.speech.create(model="tts-1", voice="nova", input=text)
-        response.stream_to_file(save_path)
+#         response = openai.audio.speech.create(model="tts-1", voice="nova", input=text)
+#         response.stream_to_file(save_path)
 
-        s3 = AmazonS3()
-        s3.upload(save_path)
+#         s3 = AmazonS3()
+#         s3.upload(save_path)
 
-        total_str_count = len(text)
-        char_cost = 0.015 / 1000
-        return total_str_count * char_cost
+#         total_str_count = len(text)
+#         char_cost = 0.015 / 1000
+#         return total_str_count * char_cost
 
 
 class AmazonS3:
@@ -94,17 +94,19 @@ async def on_message(message: discord.Message):
         api.input_message(question)
         answer = api.input_list[-1]["content"]
         await message.channel.send(answer)
-    if message.content.startswith("!読んで"):
-        question = message.content[4:]
-        if len(question) > 4096:
-            await message.channel.send("4096文字以下で入力してください。")
-            return
-        api = Text2speech()
-        cost = api.text2speech(question)
-        await message.channel.send(
-            f"今回かかったコストは${cost:.6f}でした。",
-            file=discord.File("/tmp/tts1.mp3", filename=f"tts1_{question[:15]}.mp3"),
-        )
+    # if message.content.startswith("!読んで"):
+    #     question = message.content[4:]
+    #     if len(question) > 4096:
+    #         await message.channel.send("4096文字以下で入力してください。")
+    #         return
+    #     api = Text2speech()
+    #     cost = api.text2speech(question)
+    #     await message.channel.send(
+    #         f"今回かかったコストは${cost:.6f}でした。",
+    #         file=discord.File("/tmp/tts1.mp3", filename=f"tts1_{question[:15]}.mp3"),
+    #     )
+    if message.content.startswith("!0"):
+        pass
 
 
 if __name__ == "__main__":
